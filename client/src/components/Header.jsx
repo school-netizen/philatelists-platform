d7d061f2d72447dc497a071d347083cfff395b4b
github-dev01 <devpathak108@outlook.com>
2024-09-03 08:31:26 +0530

    Header designed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,56 +2,68 @@ import React from 'react'
 import { Link, NavLink } from "react-router-dom";
 export default function Header() {
   return (
-    <header className='bg-slate-300 rounded-lg'>
-      <div className='container mx-auto p-2 flex items-center justify-between'>
+    <div className='container mx-auto outline flex items-center p-3'>
+      {/* Logo */}
+      <div className='flex gap-1 items-center w-1/3'>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="40" height="40" color="#1C1917" fill="none">
+          <path d="M2 22H22" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M15 22V16.2727C15 14.4653 13.6569 13 12 13C10.3431 13 9 14.4653 9 16.2727V22" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M17 10L20 10" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M4 10L7 10" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M11.5 10L12.5 10" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M4 22L4 6H20L20 22" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M5 6V5C5 3.58579 5 2.87868 5.43934 2.43934C5.87868 2 6.58579 2 8 2H16C17.4142 2 18.1213 2 18.5607 2.43934C19 2.87868 19 3.58579 19 5L19 6" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M3 6H21" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+        <h1 className='text-2xl text-stone-900 font-semibold'>Indian Philately</h1>
+      </div>
 
+      <div>
+        {/* Navigation Menu */}
+        <nav>
+          <ul className='flex text-sm font-medium'>
+            <li><NavLink to={'/'} className={({ isActive }) => `${isActive ? 'text-stone-900 underline underline-offset-1 decoration-stone-900' : 'text-stone-600'}`}>
+              Home
+            </NavLink></li>
+            <li>
+              <div className="hs-dropdown relative inline-flex">
+                <button id="hs-dropdown-with-icons" type="button" className="hs-dropdown-toggle py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none" aria-haspopup="menu" aria-expanded="false" aria-label="Dropdown">
+                  Actions
+                  <svg className="hs-dropdown-open:rotate-180 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6" /></svg>
+                </button>F
 
-        {/* Logo part */}
-        <div className='flex gap-3 items-center w-1/3'>
-          <Link to={"/"}>
-            <div className='size-20 bg-sky-400 rounded-full'>
-              {/* Logo  */}
-            </div>
-          </Link>
-          <Link to={"/"}>
-            <h1 className='text-2xl font-bold text-slate-800 '>
-              Indian Philately
-            </h1>
-          </Link>
-        </div>
+                <div className="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg p-1 space-y-0.5 mt-2 divide-y divide-gray-200" role="menu" aria-orientation="vertical" aria-labelledby="hs-dropdown-with-icons">
+                  <div className="py-2 first:pt-0 last:pb-0">
+                    <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100" href="#">
+                      <svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" /><path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" /></svg>
+                      Newsletter
+                    </a>
+                    <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100" href="#">
+                      <svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="8" cy="21" r="1" /><circle cx="19" cy="21" r="1" /><path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" /></svg>
+                      Purchases
+                    </a>
+                    <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100" href="#">
+                      <svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242" /><path d="M12 12v9" /><path d="m8 17 4 4 4-4" /></svg>
+                      Downloads
+                    </a>
+                    <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100" href="#">
+                      <svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" /><circle cx="9" cy="7" r="4" /><path d="M22 21v-2a4 4 0 0 0-3-3.87" /><path d="M16 3.13a4 4 0 0 1 0 7.75" /></svg>
+                      Team Account
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </nav>
 
-        <div className='flex w-2/3 justify-around items-center'>
-          <nav>
-            <ul className='flex justify-around'>
+        {/* Log-in/Sign-up */}
+        <div>
+          {/* Profile Logo */}
 
-              <NavLink to={"/"} className={({isActive})=>`${isActive ? "bg-slate-50 rounded-sm text-slate-800 ":"text-slate-500"} px-6 font-medium `}>
-              <li className=''>Home</li>
-              </NavLink>
-              <NavLink to={"/services"} className={({isActive})=>`${isActive ? "bg-slate-50 rounded-sm text-slate-800":"text-slate-500"} px-6 font-medium `}>
-              <li className=''>Services</li>
-              </NavLink>
-              <NavLink to={"/cancellations"} className={({isActive})=>`${isActive ? "bg-slate-50 rounded-sm text-slate-800  ":"text-slate-500"} px-6 font-medium `}>
-              <li className=''>Cancellations</li>
-              </NavLink>
-              <NavLink to={"/about"} className={({isActive})=>`${isActive ? "bg-slate-50 rounded-sm text-slate-800  ":"text-slate-500"} px-6 font-medium `}>
-              <li className=''>About</li>
-              </NavLink>
-              <NavLink to={"/new-events"} className={({isActive})=>`${isActive ? "bg-slate-50 rounded-sm text-slate-800  ":"text-slate-500"} px-6 font-medium `}>
-              <li className=''>News & Events</li>
-              </NavLink>
-            </ul>
-          </nav>
-
-          <div className='flex items-center gap-1'>
-          <NavLink to={"/log-in"} className={({isActive})=>`${isActive ? "bg-slate-50 rounded-sm text-slate-800  ":"text-slate-500"} px-6 font-medium list-none`}>
-              <li className=''>Log-in</li>
-              </NavLink>
-            <div className='size-9 bg-sky-400 rounded-full'>
-              {/* Profile */}
-            </div>
-          </div>
+          {/* Button */}
         </div>
       </div>
-    </header>
+    </div>
   )
 }
