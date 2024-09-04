3c9caa72c8343976a85cb75bd3fd0d36559e6489
github-dev01 <devpathak108@outlook.com>
2024-09-04 11:28:26 +0530

    some UIs are designed

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -2,8 +2,41 @@ import React from 'react'
 
 function Card() {
   return (
-    <>
-    </>
+    <div className='flex gap-3'>
+      <div class="max-w-sm w-80 overflow-hidden rounded-lg shadow-lg bg-[#2A1B12] text-white flex flex-col justify-between">
+        <img class="w-full h-48 object-cover" src="https://via.placeholder.com/300x200" alt="Product Image" />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">Product Name</div>
+          <p class="text-[#A69482] text-base">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas voluptatum consequuntur nisi vero animi. Cum, deleniti eum.
+          </p>
+        </div>
+        <div>
+          <div class="flex items-center text-[#C1B3A4] text-xl font-semibold mt-2 px-6">
+            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none">
+              <path d="M6 4H10.5M10.5 4C12.9853 4 15 6.01472 15 8.5C15 10.9853 12.9853 13 10.5 13H6L13 20M10.5 4H18M6 8.5H18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+            <span>1,299</span>
+          </div>
+          <div class="px-6 pt-4 pb-2 flex justify-between items-center">
+            <div class="space-x-2">
+              <button class="bg-[#A69482] text-[#2A1B12] hover:bg-[#C1B3A4] py-1 px-2 rounded-lg">Buy Now</button>
+              <button class="bg-[#A69482] text-[#2A1B12] hover:bg-[#C1B3A4] py-1 px-2 rounded-lg">Add to Cart</button>
+            </div>
+            <button class="text-[#C1B3A4] hover:text-[#D3C6B5]">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="currentColor" fill="none">
+                <path d="M19.4626 3.99415C16.7809 2.34923 14.4404 3.01211 13.0344 4.06801C12.4578 4.50096 12.1696 4.71743 12 4.71743C11.8304 4.71743 11.5422 4.50096 10.9656 4.06801C9.55962 3.01211 7.21909 2.34923 4.53744 3.99415C1.01807 6.15294 0.221721 13.2749 8.33953 19.2834C9.88572 20.4278 10.6588 21 12 21C13.3412 21 14.1143 20.4278 15.6605 19.2834C23.7783 13.2749 22.9819 6.15294 19.4626 3.99415Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+              </svg>
+            </button>
+            <button class="text-[#C1B3A4] hover:text-[#D3C6B5]">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,72 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export default function Signup() {
   return (
-    <div>Signup</div>
+    <div>
+      <h1 className='text-3xl text-center font-bold mt-7'>Sign Up</h1>
+      <p className='text-xs font-medium text-center'>Create your account</p>
+      <div>
+        <form action="" className='max-w-sm mx-auto mt-4 space-y-4 bg-slate-800 p-4 rounded-lg'>
+          <div className='flex bg-red-50 p-2 gap-2 rounded-lg flex-row-reverse'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} color={"#000000"} fill={"none"}>
+              <path d="M7.78256 17.1112C6.68218 17.743 3.79706 19.0331 5.55429 20.6474C6.41269 21.436 7.36872 22 8.57068 22H15.4293C16.6313 22 17.5873 21.436 18.4457 20.6474C20.2029 19.0331 17.3178 17.743 16.2174 17.1112C13.6371 15.6296 10.3629 15.6296 7.78256 17.1112Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M15.5 10C15.5 11.933 13.933 13.5 12 13.5C10.067 13.5 8.5 11.933 8.5 10C8.5 8.067 10.067 6.5 12 6.5C13.933 6.5 15.5 8.067 15.5 10Z" stroke="currentColor" strokeWidth="1.5" />
+              <path d="M2.854 16C2.30501 14.7664 2 13.401 2 11.9646C2 6.46129 6.47715 2 12 2C17.5228 2 22 6.46129 22 11.9646C22 13.401 21.695 14.7664 21.146 16" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
+            </svg>
+            <input type="text"
+              id="username"
+              placeholder='Username'
+              className='bg-transparent outline-none placeholder:text-sm placeholder:text-stone-500 w-full placeholder:font-normal font-medium'
+            />
+          </div>
+          <div className='flex bg-red-50 p-2 gap-2 rounded-lg flex-row-reverse'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} color={"#000000"} fill={"none"}>
+              <path d="M2 5L8.91302 8.92462C11.4387 10.3585 12.5613 10.3585 15.087 8.92462L22 5" stroke="currentColor" strokeWidth="1.5" strokeLinejoin="round" />
+              <path d="M10.5 19.5C10.0337 19.4939 9.56682 19.485 9.09883 19.4732C5.95033 19.3941 4.37608 19.3545 3.24496 18.2184C2.11383 17.0823 2.08114 15.5487 2.01577 12.4814C1.99475 11.4951 1.99474 10.5147 2.01576 9.52843C2.08114 6.46113 2.11382 4.92748 3.24495 3.79139C4.37608 2.6553 5.95033 2.61573 9.09882 2.53658C11.0393 2.4878 12.9607 2.48781 14.9012 2.53659C18.0497 2.61574 19.6239 2.65532 20.755 3.79141C21.8862 4.92749 21.9189 6.46114 21.9842 9.52844C21.9939 9.98251 21.9991 10.1965 21.9999 10.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M19 17C19 17.8284 18.3284 18.5 17.5 18.5C16.6716 18.5 16 17.8284 16 17C16 16.1716 16.6716 15.5 17.5 15.5C18.3284 15.5 19 16.1716 19 17ZM19 17V17.5C19 18.3284 19.6716 19 20.5 19C21.3284 19 22 18.3284 22 17.5V17C22 14.5147 19.9853 12.5 17.5 12.5C15.0147 12.5 13 14.5147 13 17C13 19.4853 15.0147 21.5 17.5 21.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
+            <input type="email" id="email"
+              placeholder='E-mail'
+              className='bg-transparent outline-none placeholder:text-sm placeholder:text-stone-500 w-full placeholder:font-normal font-medium' />
+          </div>
+          <div className='flex bg-red-50 p-2 gap-2 rounded-lg flex-row-reverse'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} color={"#000000"} fill={"none"}>
+              <path d="M4.26781 18.8447C4.49269 20.515 5.87613 21.8235 7.55966 21.9009C8.97627 21.966 10.4153 22 12 22C13.5847 22 15.0237 21.966 16.4403 21.9009C18.1239 21.8235 19.5073 20.515 19.7322 18.8447C19.879 17.7547 20 16.6376 20 15.5C20 14.3624 19.879 13.2453 19.7322 12.1553C19.5073 10.485 18.1239 9.17649 16.4403 9.09909C15.0237 9.03397 13.5847 9 12 9C10.4153 9 8.97627 9.03397 7.55966 9.09909C5.87613 9.17649 4.49269 10.485 4.26781 12.1553C4.12105 13.2453 4 14.3624 4 15.5C4 16.6376 4.12105 17.7547 4.26781 18.8447Z" stroke="currentColor" strokeWidth="1.5" />
+              <path d="M7.5 9V6.5C7.5 4.01472 9.51472 2 12 2C14.4853 2 16.5 4.01472 16.5 6.5V9" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M16 15.49V15.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M12 15.49V15.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M8 15.49V15.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
+            <input type="password" id="password"
+              placeholder='Password'
+              className='bg-transparent outline-none placeholder:text-sm placeholder:text-stone-500 w-full placeholder:font-normal font-medium' />
+          </div>
+          <div className='flex bg-red-50 p-2 gap-2 rounded-lg flex-row-reverse'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none">
+              <path d="M13.4082 16.6677C13.4082 16.6677 14.0332 16.6677 14.6582 18.001C14.6582 18.001 16.6435 14.6677 18.4082 14.001" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M16.9434 7.00098H16.9524" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M11.9434 7.00098H11.9524" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M6.94336 7.00098H6.95234" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M7.89193 11.968H5.00845C3.34693 11.968 2 10.6249 2 8.96802V4.99805C2 3.34119 3.34693 1.99805 5.00845 1.99805H18.9916C20.6531 1.99805 22 3.34119 22 4.99805V9.12895" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M21.9996 16.001C21.9996 12.6873 19.3057 10.001 15.9827 10.001C12.6597 10.001 9.96582 12.6873 9.96582 16.001C9.96582 19.3147 12.6597 22.001 15.9827 22.001C19.3057 22.001 21.9996 19.3147 21.9996 16.001Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
+            </svg>
+            <input type="password" id="confirm_pwd"
+              placeholder='Confirm Password'
+              className='bg-transparent outline-none placeholder:text-sm placeholder:text-stone-500 w-full placeholder:font-normal font-medium' />
+          </div>
+        </form>
+        <div className='max-w-sm mx-auto flex justify-center mt-2'>
+          <button type='submit' className='bg-[#2A1B12] text-sm font-medium text-white py-3 rounded-full w-1/2'>
+            Sign up
+          </button>
+        </div>
+      </div>
+      <div className='text-sm mt-6 font-medium underline decoration-4 underline-offset-4 decoration-stone-500 flex justify-center'>
+        <span className='text-stone-500'>Already have an account? &nbsp; </span> 
+        <Link to={'/log-in'}>
+          <span className=' underline decoration-4 underline-offset-4 decoration-orange-800'>Log-in</span>
+        </Link>
+      </div>
+    </div>
   )
 }
